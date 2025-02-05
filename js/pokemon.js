c129f3f1e256c1732fd895c636acf1c310e21995
Israel Avila <israel.avilaf@gmail.com>
2025-02-04 21:28:39 -0300

    Obtener pokemones

---
+++ js/pokemon.js
@@ -0,0 +1,42 @@
+let pokemones = [];
+const URL_API = `https://pokeapi.co/api/v2`;
+
+function obtenerPokemones(){
+    // 1. Hacer una petición al servidor con un metodo asincrónico con axios
+    axios.get(`${URL_API}/pokemon`).then(response =>{
+                                        console.log(`Respuesta desde pokemonapi`, response);
+
+                                        // 2. Guardar la respuesta en la variable pokemones
+                                        pokemones = response.data.results
+
+                                        // 3. Pintar los pokemones en el HTML
+                                        pintarPokemones(pokemones);
+                                    })
+                                   .catch(error => {
+                                        console.error(error);
+                                        console.error(`Error al obtener los pokemones`);
+                                   })
+}
+
+obtenerPokemones()
+
+function pintarPokemones(arrayPokemones) {
+    // 1. Seleccionar el contenedor de pokemones
+    const pokemonListHTML = document.getElementById(`pokemon-list`);
+
+    // 2. Borrar el contenido del contenedor
+    console.log(`pintarpokemones`, arrayPokemones);
+    
+    // 3. Recorrer el array de pokemones con un forEach
+    arrayPokemones.forEach(pokemon => {
+
+    })
+}
+
+
+
+
+
+function crearCardPokemon() {
+
+}
